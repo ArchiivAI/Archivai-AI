f95ac7c31374123d97dc9265dcf624a21e82805e
saadaltohamy <saad.dodo444@gmail.com>
2025-01-03 16:58:47 +0200

    first init: OCR, and Naive Classification

---
+++ app/main.py
@@ -0,0 +1,226 @@
+from ocr_functions import extract_text, gpt_ocr_layout
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from io import BytesIO
+import base64
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from pydantic import BaseModel
+import traceback
+from typing import Optional
+
+app = FastAPI(
+    title="ArchivAI THE BEST AI!",
+    description="AI API for ArchivAI",
+    version="1.0.0"
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://ccdtr14p-3000.uks1.devtunnels.ms",
+    "https://syntaxsquad-ai.azurewebsites.net"
+]
+
+# config Cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Authenticate to Azure Key Vault
+credential = DefaultAzureCredential()
+keyvault_name = "vaultarchivai"
+kv_uri = f"https://{keyvault_name}.vault.azure.net"
+keys_client = SecretClient(vault_url=kv_uri, credential=credential)
+
+# Authenticate to Azure OpenAI
+api_base = keys_client.get_secret("archivai-openai-base").value
+api_key= keys_client.get_secret("archivaigpt4-key").value
+deployment_name = 'archivaigpt4'
+api_version = '2024-08-01-preview'
+client = AzureOpenAI(
+    api_key=api_key,
+    api_version=api_version,
+    base_url=f"{api_base}/openai/deployments/{deployment_name}"
+)
+
+class classification(BaseModel):
+    target_class: str
+    accuracy: float
+
+def classify_file_bytes(file_bytes: bytes) -> str:
+    """
+    Classify an image to one of the specified categories.
+
+    :param file_bytes: Image data in bytes.
+    :return: Category of the image.
+    """
+
+    # Open the image from bytes
+    img = Image.open(BytesIO(file_bytes))
+    
+    # Create a buffer to hold the image data
+    buffer = BytesIO()
+    
+    # Save the image to the buffer in its original format
+    img_format = img.format  # Get the image format (e.g., PNG, JPEG)
+    img.save(buffer, format=img_format)
+    
+    # Encode the image data to base64
+    img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    
+    # Create the base64 string with the appropriate data URI scheme
+    img_b64_str = f"data:image/{img_format.lower()};base64,{img_base64}"
+    prompt = """
+        classify this document to one of these categories:
+['Advertisement',
+ 'Email',
+ 'Form',
+ 'Letter',
+ 'Memo',
+ 'News',
+ 'Note',
+ 'Report',
+ 'Resume',
+ 'Scientific']
+ 
+ And give me the accuracy of your decision from 0 "not sure" to 100 "Sure".
+ if the provided image does not belong to any of the categories, please provide the closest one and because your are not sure, make the accuracy low.
+    """
+    # Pass the image and the prompt to the model
+    response = client.beta.chat.completions.parse(
+        model=deployment_name,
+        messages=[
+            { "role": "system", "content": "You Are a document classification client." },
+            { "role": "user", "content": [  
+                { 
+                    "type": "text", 
+                    "text": prompt
+                },
+                { 
+                    "type": "image_url",
+                    "image_url":
+                    {
+                        "url": img_b64_str
+                    }
+                }
+            ] }
+        ],
+        max_tokens=2000,
+        response_format=classification
+    )
+    result = response.choices[0].message.parsed
+    print(result)
+
+    folder = result.target_class
+    accuracy = result.accuracy
+    section = "/SyntaxSquad/"
+    return section + folder, accuracy
+
+@app.get("/")
+def hello_world():
+    return {"message":"Hello World! OCR"}
+
+@app.post("/classify-image/")
+async def classify_image_endpoint(file: UploadFile = File(...)):
+    """
+    Endpoint to classify an uploaded image.
+
+    :param file: Image file uploaded by the user.
+    :return: JSON response with the classification result.
+    """
+    try:
+        # Validate the uploaded file's content type
+        if file.content_type not in ["image/png", "image/jpeg", "image/jpg"]:
+            raise HTTPException(status_code=400, detail="Invalid image type. Only PNG and JPEG are supported.")
+
+        # Read the image bytes
+        file_bytes = await file.read()
+
+        # Check the size of the image (limit to 5MB)
+        if len(file_bytes) > 5 * 1024 * 1024:
+            raise HTTPException(status_code=400, detail="Image size exceeds 5MB limit.")
+
+        # Classify the image
+        path, accuracy = classify_file_bytes(file_bytes)
+
+        return JSONResponse(content={"path": path, "accuracy": accuracy})
+
+    except ValueError as ve:
+        raise HTTPException(status_code=500, detail=str(ve))
+    except Exception as e:
+        # traceback_str = ''.join(traceback.format_exception(e))
+        # print(traceback_str)
+        # Log the exception details if necessary
+        raise HTTPException(status_code=500, detail=f"An error occurred while processing the image: {str(e)}")
+
+@app.post("/extract-text/")
+async def extract_text_from_image(url: str = "None", file: Optional[UploadFile] = None, is_url: bool = False):
+    """
+    Endpoint to extract text from an uploaded image or PDF.
+
+    :param url: URL of the file to be processed.
+    :param file: Image or PDF file uploaded by the user.
+    :param is_url: Flag to indicate if the file is provided as a URL.
+    :return: JSON response with the extracted text.
+    """
+    print("I'm IN!")
+    if is_url:
+        if url == "None":
+            raise HTTPException(status_code=400, detail="URL is required.")
+        try:
+            text = extract_text(file=url, url=True)
+            # return JSONResponse(content={"text": text.markdown_text, "raw_text": text.raw_text})
+        except ValueError as ve:
+            raise HTTPException(status_code=500, detail=str(ve))
+        except Exception as e:
+            traceback_str = ''.join(traceback.format_exception(e))
+            print(traceback_str)
+            raise HTTPException(status_code=500, detail=f"An error occurred while processing the URL: {str(e)}")
+
+    else:
+        if file is None:
+            raise HTTPException(status_code=400, detail="File is required if URL is not provided.")
+        
+        try:
+            # Validate the uploaded file's content type
+            if file.content_type not in ["image/png", "image/jpeg", "image/jpg", "application/pdf"]:
+                raise HTTPException(status_code=400, detail="Invalid file type. Only PNG, JPEG, and PDF are supported.")
+
+            # Read the file bytes
+            file_bytes = await file.read()
+
+            # Check the size of the file (limit to 5MB)
+            if len(file_bytes) > 5 * 1024 * 1024:
+                raise HTTPException(status_code=400, detail="File size exceeds 5MB limit.")
+
+            # Extract text from the file bytes
+            text = extract_text(file_bytes, url=False)
+
+        except ValueError as ve:
+            raise HTTPException(status_code=500, detail=str(ve))
+        except Exception as e:
+            # Log the exception details if necessary
+            traceback_str = ''.join(traceback.format_exception(e))
+            print(traceback_str)
+            raise HTTPException(status_code=500, detail=f"An error occurred while processing the file: {str(e)}")
+        
+    if len(text) == 1:
+        return JSONResponse(content={"text": text[0].markdown_text, "raw_text": text[0].raw_text})
+    else:
+        # Convert the list of page objects to a list of dictionaries
+        text_dicts = [page_obj.dict() for page_obj in text]
+        return JSONResponse(content={"pages": text_dicts})
+
+if __name__ == "__main__":
+    # Run the application with uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ app/ocr_functions.py
@@ -0,0 +1,285 @@
+from PIL import Image
+from io import BytesIO
+import base64
+from mimetypes import guess_type
+from openai import AzureOpenAI
+from pydantic import BaseModel
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from azure.storage.blob import BlobServiceClient
+import base64
+from io import BytesIO
+import fitz
+import asyncio
+import nest_asyncio
+from urllib.parse import urlparse
+import time
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import (
+    SystemMessage,
+    UserMessage,
+    TextContentItem,
+    ImageContentItem,
+    ImageUrl,
+    ImageDetailLevel,
+)
+import io
+
+credential = DefaultAzureCredential()
+keyvault_name = "vaultarchivai"
+kv_uri = f"https://{keyvault_name}.vault.azure.net"
+keys_client = SecretClient(vault_url=kv_uri, credential=credential)
+
+api_base = keys_client.get_secret("archivai-openai-base").value
+api_key= keys_client.get_secret("archivaigpt4-key").value
+deployment_name = 'archivaigpt4'
+api_version = '2024-08-01-preview'
+
+client = AzureOpenAI(
+    api_key=api_key,
+    api_version=api_version,
+    base_url=f"{api_base}/openai/deployments/{deployment_name}"
+)
+
+azure_ai_endpoint = keys_client.get_secret("azure-ai-service-endpoint").value
+key = keys_client.get_secret("azure-ai-service-key").value
+
+class Page(BaseModel):
+    markdown_text: str
+    raw_text: str
+
+nest_asyncio.apply()
+system_prompt = "Your are an OCR client using your Vision Capabilities to perform your response and provide a clean and structured text without any notes."
+user_prompt = """extract the text from the image and provide a clean and structured text. 
+                your output format must be in markdown format, with the text extracted from the image. 
+                output will be  in 'markdown' text with markdown rules like titles,headings,etc, nothing else.
+                
+                NOTES:
+                1) in the output, don't write ```markdown``` or ```md``` or any other code block.
+                
+                2) don't add any notes from you, just out the text extracted from the image. without any additions
+                
+                3) Warning! Don't Add Notice section to tell me that the text is not clear or any other notes.
+
+                4) even if the text is not clear, try to extract as much as possible, and don't provide any extra notes.
+                5) output the raw text extracted from the image as well. this text will be used for embedding purposes.
+                6) Extract the text in the language it is written in the image.
+                7) I will provide you multiple images, extract the text from all the images and provide the output in the same format.
+
+                """
+
+def download_pdf(blob_client):
+    pdf_bytes = blob_client.download_blob().readall()
+    return pdf_bytes
+
+def generate_messages(system_prompt, user_prompt, image_urls):
+    return [[
+    {"role": "system", "content": system_prompt},
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": user_prompt
+            },
+            # Add one dict per image url
+            (
+                {
+                    "type": "image_url",
+                    "image_url": {"url": url}
+                }
+            )
+        ],
+    }]
+    for url in image_urls]
+
+def task(message):
+    completion = client.beta.chat.completions.parse(
+        model=deployment_name,
+        messages=message,
+        response_format=Page,
+        )
+    return completion.choices[0].message.parsed
+
+def convert_pdf_to_images(pdf_path, url=False, dpi=300):
+    # Open the PDF file
+    if url:
+        pdf_document = fitz.open(stream=download_pdf(pdf_path), filetype="pdf")
+    else:
+        # if pdf_path is a string file path
+        if isinstance(pdf_path, str):
+            pdf_document = fitz.open(pdf_path)
+        else:
+            pdf_document = fitz.open(stream=pdf_path, filetype="pdf")
+    images = []
+
+    # Calculate the zoom factor based on the desired DPI
+    zoom = dpi / 72  # 72 is the default DPI for PDFs
+    mat = fitz.Matrix(zoom, zoom)
+
+    # Iterate through each page
+    for page_num in range(len(pdf_document)):
+        page = pdf_document.load_page(page_num)
+        pix = page.get_pixmap(matrix=mat)
+        
+        # Convert to PIL Image
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        images.append(img)
+
+    return images
+
+def image_pil_to_data_url(pil_image, mime_type="image/png"):
+    """
+    Converts a PIL Image into a base64-encoded data URL string.
+    """
+    buffer = BytesIO()
+    pil_image.save(buffer, format="PNG")  # or "JPEG"
+    base64_data = base64.b64encode(buffer.getvalue()).decode("utf-8")
+    return f"data:{mime_type};base64,{base64_data}"
+
+def pdf_to_image_urls(pdf_file_path, url=False):
+    """
+    1. Converts each PDF page to a PIL Image.
+    2. Converts each image to a data URL string.
+    3. Returns a list of these data URLs.
+    """
+    # Convert each page in the PDF to a PIL image
+    pil_images = convert_pdf_to_images(pdf_file_path, url, dpi=100)
+    # Convert each PIL image to a Base64 data URL
+    image_urls = [
+        image_pil_to_data_url(pil_img, mime_type="image/png")
+        for pil_img in pil_images
+    ]
+    return image_urls
+
+async def main(messages):
+    tasks = [asyncio.to_thread(task, message) for message in messages]
+    result = await asyncio.gather(*tasks)
+    return result
+
+def pdf_ocr(pdf_path, url=False):
+    time_start = time.time()
+    images = pdf_to_image_urls(pdf_path, url)
+    messages = generate_messages(system_prompt, user_prompt, images)
+    time_end = time.time()
+    print(f"Generated {len(images)} messages in {time_end - time_start:.2f} seconds")
+    
+    time_start = time.time()
+    final_result = asyncio.run(main(messages))
+    time_end = time.time()
+    print(f"Completed OCR in {time_end - time_start:.2f} seconds")
+    
+    return final_result 
+
+def blob_to_base64(blob):
+    image_bytes = blob.download_blob().readall()
+    image = Image.open(BytesIO(image_bytes))
+    imaeg_url = image_pil_to_data_url(image)
+    return imaeg_url
+    # buffered = BytesIO()
+    # image.save(buffered, format="PNG")
+    # img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    # return img_str
+
+def image_ocr(blob,url=False):
+    if url:
+        image_url = blob_to_base64(blob)
+    else:
+        image_url = image_pil_to_data_url(Image.open(blob))
+    messages = generate_messages(system_prompt, user_prompt, [image_url])
+    final_result = asyncio.run(main(messages))
+    return final_result
+
+def extract_text(file, url=False):
+    if url:
+        parsed_url = urlparse(file)
+        path_parts = parsed_url.path.lstrip('/').split('/')
+        container_name = path_parts[0]
+        blob_name = '/'.join(path_parts[1:])
+    
+        blob_service_client = BlobServiceClient.from_connection_string(keys_client.get_secret("blob-connection-string").value)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        
+        blob_content = blob_client.get_blob_properties().content_settings.content_type
+        if blob_content == "application/pdf":
+            result = pdf_ocr(blob_client, url=url)
+        elif blob_content.startswith("image/"):
+            result = image_ocr(blob_client, url=url)
+    
+    else:
+        if isinstance(file, bytes):
+            # Determine the file type from the bytes
+            file_type = None
+            try:
+                # Try to open as an image
+                Image.open(io.BytesIO(file))
+                file_type = "image"
+            except IOError:
+                # If it fails, try to open as a PDF
+                try:
+                    fitz.open(stream=file, filetype="pdf")
+                    file_type = "pdf"
+                except:
+                    raise ValueError("Unsupported file type")
+
+            if file_type == "pdf":
+                result = pdf_ocr(file, url=url)
+            elif file_type == "image":
+                result = image_ocr(io.BytesIO(file), url=url)
+        else:
+            if file.endswith(".pdf"):
+                result = pdf_ocr(file)
+            elif file.endswith((".png", ".jpg", ".jpeg")):
+                result = image_ocr(file)
+    return result
+
+def vlm_ocr(path, model_deployment, endpoint):
+    image_path = path
+    vlm_client = ChatCompletionsClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(key),
+        headers={"azureml-model-deployment": model_deployment},
+    )
+
+    response = vlm_client.complete(
+        messages=[
+            SystemMessage(content=system_prompt),
+            UserMessage(content=[
+                TextContentItem(text=user_prompt),
+                ImageContentItem(
+                    image_url=ImageUrl(
+                        url=image_path,
+                        detail=ImageDetailLevel.HIGH,
+                    )
+                )
+            ])
+        ],
+        model=model_deployment
+    )
+    return response.choices[0].message.content
+
+def gpt_ocr_layout(image_url):
+    completion = client.beta.chat.completions.parse(
+        model=deployment_name,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": [
+                {
+                    "type": "text",
+                    "text": user_prompt
+                },
+                {
+                    "type": "image_url",
+                    "image_url":
+                        {
+                            "url": image_url
+                        }
+                }
+            ]},
+        ],
+        response_format=Page,
+    )
+
+    result = completion.choices[0].message.parsed
+    return result
